212751cfbbf9acb8641c53c03d8eec4d67c61230
MakerSquare Student <students@themakersquare.com>
2013-08-02 13:45:58 -0500

    Wrote a failing argument error test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,13 +7,17 @@ describe Message do
     context 'when both the sender and recipient are specified' do
       it 'should set the sender of the message'
 
+
       it 'should set the recipient of the message'
 
       it 'should persist the message to the database'
     end
 
     context 'when neither the sender or recipient is specified' do
-      it 'should raise an error'
+      it 'should raise an error' do
+        expect { message.deliver! }.to raise_error ArgumentError
+
+      end
     end
 
     context 'when only the sender is specified' do
